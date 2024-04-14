2f24e84c533d406e1db046584f3ce3981c0d8d4a
Q.A.zh <40236765+QAbot-zh@users.noreply.github.com>
2024-04-15 00:35:54 +0800

    Update openai.ts（modify max_tokens）

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -106,18 +106,19 @@ export class ChatGPTApi implements LLMApi {
       presence_penalty: modelConfig.presence_penalty,
       frequency_penalty: modelConfig.frequency_penalty,
       top_p: modelConfig.top_p,
-      // max_tokens: Math.max(modelConfig.max_tokens, 1024),
+      max_tokens: Math.max(modelConfig.max_tokens, 1024),
       // Please do not ask me why not send max_tokens, no reason, this param is just shit, I dont want to explain anymore.
     };
 
     // add max_tokens to vision model
     if (visionModel) {
-      Object.defineProperty(requestPayload, "max_tokens", {
-        enumerable: true,
-        configurable: true,
-        writable: true,
-        value: modelConfig.max_tokens,
-      });
+      requestPayload["max_tokens"] = Math.max(modelConfig.max_tokens, 4000);
+      // Object.defineProperty(requestPayload, "max_tokens", {
+      //   enumerable: true,
+      //   configurable: true,
+      //   writable: true,
+      //   value: modelConfig.max_tokens,
+      // });
     }
 
     console.log("[Request] openai payload: ", requestPayload);

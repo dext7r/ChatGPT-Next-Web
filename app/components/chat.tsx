86a0cdf15190188c31f649655ea765237ab2d32c
undefinedcodezhong <40236765+undefinedcodezhong@users.noreply.github.com>
2024-08-30 10:42:39 +0800

    屏蔽右键复制内容到输入区域

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1419,7 +1419,7 @@ function _Chat() {
                         message.content.length === 0 &&
                         !isUser
                       }
-                      onContextMenu={(e) => onRightClick(e, message)}
+                      // onContextMenu={(e) => onRightClick(e, message)}  //don't copy message to input area when right click
                       onDoubleClickCapture={() => {
                         if (!isMobileScreen) return;
                         setUserInput(getMessageTextContent(message));

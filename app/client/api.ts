2178fd6c34f2ff2ff844736c12da3d24afb76fc8
undefinedcodezhong <40236765+undefinedcodezhong@users.noreply.github.com>
2025-07-01 14:07:07 +0800

    修改主页 oai logo 规避风险 #6545

--- app/client/api.ts
+++ app/client/api.ts
@@ -19,7 +19,7 @@ export const ROLES = ["system", "user", "assistant"] as const;
 export type MessageRole = (typeof ROLES)[number];
 
 export const Models = ["gpt-3.5-turbo", "gpt-4"] as const;
-export const TTSModels = ["tts-1", "tts-1-hd"] as const;
+export const TTSModels = ["gpt-4o-mini-tts", "tts-1", "tts-1-hd"] as const;
 export type ChatModel = ModelType;
 
 export interface MultimodalContent {
